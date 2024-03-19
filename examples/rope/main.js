751fdd6764bc8cb2c68cb422ce073fdb5c9b8527
0m0g1 <omogi.js@gmail.com>
2024-03-19 22:09:13 +0300

    rope

---
+++ examples/rope/main.js
@@ -0,0 +1,21 @@
+import YapheEngine from "../../src/engine.js";
+
+const engine = new YapheEngine({element: "#yaphe-simulation"});
+const ropeWorld = engine.createWorld2d();
+ropeWorld.createCanvas();
+
+const y = ropeWorld.center.y - 200;
+const particles = [];
+
+for (let i = 0; i < 10; i++) {
+    const particle = ropeWorld.createParticle2D(undefined, y, false);
+    particle.radius = 4;
+    particles.push(particle);
+}
+
+for (let i = 0; i < particles.length - 1; i++) {
+    const spring = ropeWorld.createSpring2D(particles[i], particles[i + 1]);
+}
+
+
+engine.ignite();
