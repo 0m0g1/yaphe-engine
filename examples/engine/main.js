2bd2dd568b4792a41589b73052d5f81d0139613c
0m0g1 <omogi.js@gmail.com>
2024-03-25 01:15:20 +0300

    engine polishing

--- examples/engine/main.js
+++ examples/engine/main.js
@@ -5,15 +5,36 @@ const engineWorld = engine.createWorld2d();
 engineWorld.createCanvas();
 
 const a = engineWorld.createParticle2D(engineWorld.center.x, engineWorld.center.y - 100);
-const b = engineWorld.createParticle2D(engineWorld.center.x, engineWorld.center.y + 100);
+const b = engineWorld.createParticle2D(engineWorld.center.x, engineWorld.center.y - 50);
+const c = engineWorld.createParticle2D(engineWorld.center.x, engineWorld.center.y);
+const d = engineWorld.createParticle2D(engineWorld.center.x, engineWorld.center.y + 50);
 engineWorld.createConstraint2D(a, b);
+engineWorld.createConstraint2D(b, c);
+engineWorld.createConstraint2D(c, d);
+
+const e = engineWorld.createParticle2D(engineWorld.center.x + 50, engineWorld.center.y + 50);
+const f = engineWorld.createParticle2D(engineWorld.center.x + 50, engineWorld.center.y + 100);
+const g = engineWorld.createParticle2D(engineWorld.center.x, engineWorld.center.y + 100);
+
+const particles = [a, b, c, d, e, f , g];
+particles.forEach((particle) => {
+    particle.isPoint = true;
+})
+
+
+engineWorld.createConstraint2D(d, e);
+engineWorld.createConstraint2D(e, f);
+engineWorld.createConstraint2D(f, g);
+engineWorld.createConstraint2D(g, d);
+engineWorld.createConstraint2D(d, f).style.visible = false;
+engineWorld.createConstraint2D(e, g).style.visible = false;
 
 
 const enginePath = engineWorld.createPath2D();
 
 const polarEquation = (angle) => {
     const x =  100 * Math.cos(angle) + engineWorld.center.x;
-    const y =  100 * Math.sin(angle) + engineWorld.center.y - 100;
+    const y =  100 * Math.sin(angle) + engineWorld.center.y - 150;
     return {x, y};
 }
 
@@ -30,9 +51,6 @@ const updateFunction = (engine) => {
     engine.angle %= Math.PI * 2;
     return engine.path.getPointAt(engine.angle);
 }
-
-engineMortar.position.x = engineWorld.center.x;
-engineMortar.position.y = engineWorld.center.y;
 
 engineMortar.setUpdateFunction(updateFunction);
 
--- src/modules/2d/engine2d.js
+++ src/modules/2d/engine2d.js
@@ -1,9 +1,10 @@
+import Particle2D from "./particle2d.js";
 import YaphePath2D from "./path2d.js";
 import Vector2D from "./vector2d.js";
 
 class Engine2D{
   constructor(particles = [], path = new YaphePath2D()){
-    this.position = new Vector2D();
+    this.particle = new Particle2D();
     this.attachedParticles = particles;
     this.path = path;
     this.updateFunction = null;
@@ -13,17 +14,15 @@ class Engine2D{
   }
   update() {
     const point = this.updateFunction(this);
-    this.position.x = point.x;
-    this.position.y = point.y;
+    this.particle.position.x = point.x;
+    this.particle.position.y = point.y;
     this.attachedParticles.forEach((particle) => {
         particle.position.x = point.x;
         particle.position.y = point.y;
     });
   }
   show(pen) {
-    pen.beginPath();
-    pen.arc(this.position.x, this.position.y, 20, 0, 2 * Math.PI);
-    pen.fill();
+    this.particle.show(pen);
     this.path.show(pen);
   }
 }
